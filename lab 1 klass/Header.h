05c91ed967ed958cb6928b2ee5604aed0ca4d112
Nikita01020 <93367159+Nikita01020@users.noreply.github.com>
2023-04-23 01:21:55 +0500

    Add files via upload

---
+++ lab 1 klass/Header.h
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+class xy
+{
+private:
+	double A;
+	double B;
+	double y;
+	double x;
+public:
+	xy();
+	xy(double A, double B, double y);
+	void Read(double A, double B, double y);
+	void SetA(double A);
+	void SetB(double B);
+	void SetY(double y);
+	double Make_Result();
+	void print();
+};
